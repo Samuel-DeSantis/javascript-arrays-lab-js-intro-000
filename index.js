82d9db43bd855f2cea92e5f088c19f234183d048
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-14 05:01:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ function destructivelyRemoveLastKitten() {
   return kittens;
 }
 function appendKitten(name) {return kittens.concat(name);}
-function prependKitten(name) {
-  return name.concat(kittens);
-}
+function prependKitten(name) {return name.concat(kittens);}
 function removeLastKitten() {return kittens.slice(0, kittens.length - 1);}
 function removeFirstKitten() {return kittens.slice(-kittens.length + 1);}
